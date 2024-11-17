32bc8af2ef981d6032cf53adcba42c68aa812057
Muhammad Tanveer <muhammadtanveer0135@gmail.com>
2024-11-17 21:02:10 +0500

    Your commit message

--- src/app/home/components/Gallery.tsx
+++ src/app/home/components/Gallery.tsx
@@ -90,7 +90,7 @@ export default function GallerySection() {
       {/* Modal */}
       {currentIndex !== null && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
-          <div className="relative w-[70%] h-[70%]">
+          <div className="relative w-[100%] h-[100%]">
             <button
               className="absolute top-4 right-4 text-white text-2xl font-bold z-10"
               onClick={closeModal}
